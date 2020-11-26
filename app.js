a9094ff8426ff2ce14fa6b79a49a368ce3aa0c0a
Shambhavi Kurup <shambhavikurup@Shambhavis-MacBook-Air.local>
2020-11-27 04:14:53 +0530

    add temporary weather checker

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 require("dotenv").config();
 const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -141,6 +142,25 @@ app.post("/login", function(req, res){
   });
 });
 
+app.post("/weather-form", function(req,res){
+  const api_key = process.env.API_KEY;
+  const selectedCity = req.body.weatherCity;
+  const units = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=" + process.env.API_KEY + "&units=" + units;
+  https.get(url,function(response){
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const weatherIcon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+      res.write("<h1>The temperature in " + selectedCity + " is " + temp + " degrees Fahrenheit.</h1>")
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<img src=" + imageURL + " alt='image'>");
+      res.send();
+    });
+  });
+});
 
 
 
